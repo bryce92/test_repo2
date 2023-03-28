7e50fcab3eb3bce8a930601c3be8b74f65529e1e
JamesMDe <James.M.DeBenedictis@gmail.com>
2023-03-28 15:50:51 -0400

    Created string variable for print statement

--- src/test_repo2/HelloWorld.java
+++ src/test_repo2/HelloWorld.java
@@ -1,7 +1,9 @@
 package test_repo2;
 
 public class HelloWorld {
+
+	String bryceMessage = "gitHub is awesome ";
 	public static void main(String[] args) {
-		System.out.print("gitHub is awesome ");
+		System.out.print(this.bryceMessage);
 	}
 }
